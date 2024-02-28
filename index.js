7ce670178038d33beecac068b45e87d30081546b
AASanusi <alaminm.sanusi@gmail.com>
2024-02-28 20:05:22 +0000

    Add then, catch and json method

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // fetch = Function used for making HTTP requests - fetch function is promised based it's wither going to resolve or reject
 
-// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-//     .then(response => console.log(response))
-//     .catch(error => console.error(error));
-
-console.log('Hello World');
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
