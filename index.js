16139c37923abfab290f5b72bb07bc5303957351
AASanusi <alaminm.sanusi@gmail.com>
2024-02-28 19:42:25 +0000

    Add html

---
+++ index.js
@@ -0,0 +1,7 @@
+// fetch = Function used for making HTTP requests - fetch function is promised based it's wither going to resolve or reject
+
+// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+//     .then(response => console.log(response))
+//     .catch(error => console.error(error));
+
+console.log('Hello World');
