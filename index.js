7a499ab6cb429363fa1720945ab07deb3bfad99d
AASanusi <alaminm.sanusi@gmail.com>
2024-02-28 20:30:54 +0000

    Add statement to catch errors

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // fetch = Function used for making HTTP requests - fetch function is promised based it's wither going to resolve or reject
 
 fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-    .then(response => response.json())
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("Could not fetch resource");
+        }
+    })
     .then(data => console.log(data))
-    .catch(error => console.error(error));
+    .catch(error => console.log(error));
+
